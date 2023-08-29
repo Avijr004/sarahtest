2c5de86187a30da706dfd99b293ca86729362c0b
Avijr004 <128925724+Avijr004@users.noreply.github.com>
2023-08-29 22:23:03 +0530

    voice_module.py

---
+++ ShizukaXMusic/plugins/modules/voice_module.py
@@ -0,0 +1,31 @@
+from pyrogram import filters
+from google.cloud import speech
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+class VoiceModule:
+    def __init__(self, bot, speech_client, spotify_client):
+        self.bot = bot
+        self.speech_client = speech_client
+        self.spotify_client = spotify_client
+
+        self.bot.add_handler(filters.voice & filters.private, self.handle_voice_command)
+
+    async def handle_voice_command(self, client, message):
+        voice_message = message.voice
+        file_path = await client.download_media(voice_message)
+        
+        recognized_text = self.recognize_speech(file_path)
+        response = self.process_voice_command(recognized_text)
+        
+        await message.reply_text(response)
+
+    def recognize_speech(self, file_path):
+        # ... Speech-to-Text code ...
+
+    def process_voice_command(self, text):
+        # ... Command processing code ...
+
+def create_voice_module(bot, speech_client, spotify_client):
+    return VoiceModule(bot, speech_client, spotify_client)
